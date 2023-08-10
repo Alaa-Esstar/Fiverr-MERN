059873964b5cfca439b1ad9c4934fe47fcff6d1f
Alaa Esstar <alaa.esstar@gmail.com>
2023-08-10 12:08:13 +0100

    add, message, messages, myGigs, orders UI

--- client/src/pages/message/Message.jsx
+++ client/src/pages/message/Message.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import "./Message.scss"
+import { Link } from "react-router-dom"
 
 const Message = () => {
     return (
-        <div>Message</div>
+        <div className='message'>
+            <div className="container">
+                <span className="breadcrumbs">
+                    <Link to="/messages" className='link'>MESSAGES</Link> - JOHN DOE
+                </span>
+                <div className="messages">
+                    <div className="item">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                    <div className="item owner">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                    <div className="item">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                    <div className="item owner">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                    <div className="item">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                    <div className="item owner">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                    <div className="item">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                    <div className="item owner">
+                        <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem minima, animi aperiam dignissimos labore ipsam repellendus eius facere harum consequatur dolorem similique quos? Ut quas odit necessitatibus dolores reiciendis!</p>
+                    </div>
+                </div>
+                <hr />
+                <div className="write">
+                    <textarea name="" placeholder='write a message' id="" cols="30" rows="10"></textarea>
+                    <button>Send</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -1,9 +1,97 @@
 import React from 'react'
-import "./Messages"
+import "./Messages.scss"
+import { Link } from "react-router-dom"
 
 const Messages = () => {
+    const message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ad nihil quos et quibusdam. Iste doloribus omnis vel! Quia iusto praesentium autem soluta nesciunt quo nostrum at eos quis vero"
     return (
-        <div>Messages</div>
+        <div className='messages'>
+            <div className="container">
+                <div className="title">
+                    <h1>Messages</h1>
+                </div>
+                <table>
+                    <tr>
+                        <th>Buyer</th>
+                        <th>Last Message</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr className='active'>
+                        <td>
+                            John Doe
+                        </td>
+                        <td>
+                            <Link className='link' to="/message/123">
+                                {message.substring(0, 100)} ...
+                            </Link>
+                        </td>
+                        <td>1 day ago</td>
+                        <td>
+                            <button>Mark as Read</button>
+                        </td>
+                    </tr>
+                    <tr className='active'>
+                        <td>
+                            John Doe
+                        </td>
+                        <td>
+                            <Link className='link' to="/message/123">
+                                {message.substring(0, 100)} ...
+                            </Link>
+                        </td>
+                        <td>1 day ago</td>
+                        <td>
+                            <button>Mark as Read</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            John Doe
+                        </td>
+                        <td>
+                            <Link className='link' to="/message/123">
+                                {message.substring(0, 100)} ...
+                            </Link>
+                        </td>
+                        <td>1 day ago</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            John Doe
+                        </td>
+                        <td>
+                            <Link className='link' to="/message/123">
+                                {message.substring(0, 100)} ...
+                            </Link>
+                        </td>
+                        <td>1 day ago</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            John Doe
+                        </td>
+                        <td>
+                            <Link className='link' to="/message/123">
+                                {message.substring(0, 100)} ...
+                            </Link>
+                        </td>
+                        <td>1 day ago</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            John Doe
+                        </td>
+                        <td>
+                            <Link className='link' to="/message/123">
+                                {message.substring(0, 100)} ...
+                            </Link>
+                        </td>
+                        <td>1 day ago</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     )
 }
 
